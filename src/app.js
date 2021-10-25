9041e6cf67e5a3726a577d4ef9c9a88c9f27b936
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2021-10-26 01:29:41 +0200

    npm run deploy

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import ProfileList from './components/Profile/profileList';
-import Users from './user.json';
+import Users from './components/Profile/user.json';
 
 import StatisticalData from './components/Statistics/statistical-data.json';
 import StatisticsList from './components/Statistics/statisticsList';
@@ -7,12 +7,16 @@ import StatisticsList from './components/Statistics/statisticsList';
 import FriendList from './components/FriendList/friendList';
 import Friends from './components/FriendList/friends.json';
 
+import TransactionHistory from './components/TransactionHistory/transactionHistory'
+import Transactions from './components/TransactionHistory/transactions.json'
+
 export default function App() {
     return (
       <div>
         <ProfileList items={Users} />
         <StatisticsList title="Upload stats" stats={StatisticalData} />
-        <FriendList friends={Friends }/>
+        <FriendList friends={Friends} />
+        <TransactionHistory items={Transactions} />
       </div>
     );
 }
