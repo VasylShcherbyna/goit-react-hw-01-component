1a276db0a1086bab0bbfd7f83e0f7794684672cc
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2021-10-30 10:44:33 +0200

    json and defoltProps

--- src/app.js
+++ src/app.js
@@ -1,28 +1,28 @@
 import Profile from './components/Profile/profile';
-import user from './components/user.json';
+import user from './data/user.json';
 
-import StatisticalData from './components/statistical-data.json';
+import StatisticalData from './data/statistical-data.json';
 import StatisticsList from './components/Statistics/statisticsList';
 
 import FriendList from './components/FriendList/friendList';
-import Friends from './components/friends.json';
+import Friends from './data/friends.json';
 
-import TransactionHistory from './components/TransactionHistory/transactionHistory'
-import Transactions from './components/transactions.json'
+import TransactionHistory from './components/TransactionHistory/transactionHistory';
+import Transactions from './data/transactions.json';
 
 export default function App() {
-    return (
-      <div>
-        <Profile
-          name={user.name}
-          tag={user.tag}
-          Location={user.Location}
-          avatar={user.avatar}
-          stats={user.stats}
-        />
-        <StatisticsList title="Upload stats" stats={StatisticalData} />
-        <FriendList friends={Friends} />
-        <TransactionHistory items={Transactions} />
-      </div>
-    );
+  return (
+    <div>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        Location={user.Location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <StatisticsList title="Upload stats" stats={StatisticalData} />
+      <FriendList friends={Friends} />
+      <TransactionHistory items={Transactions} />
+    </div>
+  );
 }
--- src/components/Statistics/statisticsList.js
+++ src/components/Statistics/statisticsList.js
@@ -2,10 +2,10 @@ import PropTypes from 'prop-types';
 import s from './statistics.module.css';
 import Statistics from './statistics';
 
-export default function StatisticsList({ stats }) {
+export default function StatisticsList({ stats, title }) {
   return (
     <section className={s.statistics}>
-      <h2 className={s.title}>Upload stats</h2>
+      <h2 className={s.title}>{ title}</h2>
       <ul className={s.statList}>
         {stats &&
           stats.map(({ id, label, percentage }) => (
@@ -17,7 +17,9 @@ export default function StatisticsList({ stats }) {
     </section>
   );
 };
-
+Statistics.defaultProps = {
+  title: 'Upload stats',
+};
 StatisticsList.propTypes = {
   stats: PropTypes.arrayOf(
     PropTypes.shape({
